c332089189bc8ab1f26167658a24bced9c07b966
theactualdev <olayinkacodes@gmail.com>
2025-03-24 18:41:18 +0100

    init: initializes index files & makes init changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,22 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { IBM_Plex_Sans, Lora, Poppins } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const ibm = IBM_Plex_Sans({
+  weight: '600',
   subsets: ["latin"],
-});
+  variable: "--font-ibm"
+})
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const lora = Lora({
   subsets: ["latin"],
-});
+  variable: "--font-lora"
+})
+const poppins = Poppins({
+  weight: '300',
+  subsets: ["latin"],
+  variable: "--font-poppins"
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`antialiased`}
       >
         {children}
       </body>
