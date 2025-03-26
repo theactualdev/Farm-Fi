ca3a7095c90e2e294b4132ded1670ad83b4fc242
theactualdev <olayinkacodes@gmail.com>
2025-03-26 12:16:44 +0100

    fix: console log

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -16,7 +16,7 @@ export default function Login() {
     password: string;
   }) => {
     try {
-        // console.log('tsar')
+        console.log('start')
       const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}auth/login`, {
         method: "POST",
         headers: {
